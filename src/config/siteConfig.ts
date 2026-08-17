b4bccc27c13b93a3b0ce8fac4b268323af9889b7
Halory <2082004203@qq.com>
2026-08-17 21:38:35 +0800

    新增了 或许以后不会走开发了.md 文件 & 修复了 sitemap

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -46,7 +46,7 @@ export const siteConfig: SiteConfig = {
   subtitle: "Life will change.",
 
   // 站点 URL
-  site_url: "https://blog.halory.fun",
+  site_url: "https://shizuku.halory.fun",
 
   // 站点描述
   description: "若爱你就来，不爱莫猖狂",
@@ -212,7 +212,7 @@ export const siteConfig: SiteConfig = {
     // 提醒框（Admonitions）配置，修改后需要重启开发服务器才能生效
     // 主题：'github' | 'obsidian' | 'vitepress' | 'docusaurus'，每个主题风格和语法不同，可根据喜好选择
     rehypeCallouts: {
-      theme: "github",
+      theme: "vitepress",
       // 是否启用兼容 Python-Markdown 风格的 admonition 语法（!!!和???语法）
       // 注意：只有 theme 配置成 obsidian 主题才能基本支持这些语法，其他主题会有样式问题或不兼容的情况
       enablePythonMarkdownAdmonitions: false,
